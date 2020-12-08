aefd9e7cb703d324f5c21badb99c41c1059810a0
Распутин Филипп Михайлович <Rasputin.FM@gazprom-neft.local>
2020-12-08 13:29:27 +0300

    Add comment and remove shebang

--- vars/logMeIn.groovy
+++ vars/logMeIn.groovy
@@ -1,7 +1,10 @@
-#!groovy
-
 import com.gpn.pipeline.LoginUtils
 
+/**
+* Executes a login function based on place
+* @param place String place where you want to login
+* @param parameters Map with specific for place parameters. For now registryCred is for Jenkins credId and registry or urlTarget for path of place
+*/
 def call(String place, Map parameters = [:]) {
     LoginUtils logUtils = new LoginUtils(this)
 
