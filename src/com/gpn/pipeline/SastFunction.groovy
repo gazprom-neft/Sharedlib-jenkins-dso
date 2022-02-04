bde33e6ae1013bdf1e2009ae860df203206a9d3f
Eugene Khokhlov <38161973+rageofgods@users.noreply.github.com>
2022-02-04 13:15:30 +0300

    Update SastFunction.groovy

--- src/com/gpn/pipeline/SastFunction.groovy
+++ src/com/gpn/pipeline/SastFunction.groovy
@@ -26,12 +26,12 @@ class SastFunction {
         return git_full_path_with_repo_name
     }
 
-    // Search across ADS collections for a requested project string and returns project id if found some one
+    // Search across ADS collections for a requested project string and returns project 'id, name' if found some one
     public def getGitProjects(user_name, git_api_proto, git_api_port, git_base_url, git_collection_path, git_project_name, git_project_req_elements, git_api_url_prefix, git_api_cred_id, is_debug) {
         def project_id = script.sh(script: """curl -su :${git_api_cred_id} '${git_api_proto}://${user_name}@${git_base_url}:${git_api_port}${git_collection_path}${git_api_url_prefix}projects?\$top=${git_project_req_elements}' | \
         jq -r '.value[] | \
         select(.name|test("^${git_project_name}.*?(?=\\\\\$|\$)"; "i")) | \
-        .id'""", returnStdout: true).trim()
+        .id,.name'""", returnStdout: true).trim()
 
         // Generate error if nothing found
         if (project_id == "" || project_id == null) {
@@ -40,18 +40,23 @@ class SastFunction {
             script.error("Aborting the build.")
         }
 
-        // Generate error if found more than one project id 
-        if (project_id.split('\n').size() > 1) {
+        // Generate error if found more than one project with 'id, name'
+        def project_id_name = project_id.split('\n')
+        def p_id, p_name
+        if (project_id_name.size() > 2) {
             script.echo("Error: found more than one project with id ${git_project_name}. Please refine your querry.")
             script.currentBuild.result = "ABORTED"
             script.error("Aborting the build.")
+        } else if (project_id_name.size() == 2) {
+            p_id = project_id_name[0]
+            p_name = project_id_name[1]
         }
 
         if (is_debug) {
             script.echo(project_id)
         }
 
-        return project_id
+        return [p_id, p_name]
     }
 
     // Returns all project repos
@@ -139,6 +144,80 @@ class SastFunction {
             script.USER = script.env.BUILD_USER ?: "Jenkins"
             script.USER_EMAIL = script.env.BUILD_USER_EMAIL
             script.buildDescription "SAST check selected repo(s): ${repos}\nBuild started by ${script.USER}\nExecuted @ ${script.env.NODE_NAME}."
+        }
+    }
+
+    // Generate SAST reports
+    public def genSastReport(sast_srv_cred_id, sast_project_name, email_list) {
+        // Default image name for SAST report generation
+        // Source of image build process: https://alm-itsk.gazprom-neft.local:8080/TFS/GPN/DSO_SERVICE/_git/scripts?path=%2Fjobs%2Fsast&version=GBmaster
+        def sast_image_name = "sast-gen-report"
+        // Default image tag                              
+        def sast_image_tag = "latest"
+        // Name of directory where sast reports will be saved                                         
+        def reports_dir = "sast-reports"
+        // Setup export reports path based on current workspace                                     
+        def sast_reports_output_dir = "${script.env.WORKSPACE}/${reports_dir}"
+
+
+        // Setup docker registry/cred vars based on current running environment
+        def sast_image_registry = ""
+        def sast_image_registry_cred_id = ""
+        switch(script.env.BUILD_ENVIRONMENT) {
+            case "dev":
+                sast_image_registry = script.env.REPO_DEV_DOCKER_REGISTRY
+                sast_image_registry_cred_id = script.env.REPO_DEV_DOCKER_REGISTRY_CRED_ID
+                break
+            case "trust":
+                sast_image_registry = script.env.REPO_TRUST_DOCKER_REGISTRY
+                sast_image_registry_cred_id = script.env.REPO_TRUST_DOCKER_REGISTRY_CRED_ID
+                break
+            default:
+                script.echo("Error: You must define 'BUILD_ENVIRONMENT' variable strictly as 'trust or 'dev'")
+                script.currentBuild.result = "ABORTED"
+                script.error("Aborting the build.")
+                break
+        }
+
+        script.dir(reports_dir) {
+            // Spawn docker container at current execution agent
+            script.withCredentials([script.usernamePassword(
+                credentialsId: sast_srv_cred_id,
+                usernameVariable: 'USERNAME',
+                passwordVariable: 'PASSWORD'
+            )]) {
+                script.docker.withRegistry("https://${sast_image_registry}", "${sast_image_registry_cred_id}") {
+                        script.docker.image("${sast_image_name}:${sast_image_tag}").inside("-e REQUESTS_CA_BUNDLE=/etc/pki/tls/cert.pem") {
+                            // We don't care about passing credentials through docker container environment variables here (in more secure way) 
+                            // because they will be exposed in Jenkins console log as docker '-e' parameters anyway
+                            script.sh """
+                            python3 /app/app.py -u $script.USERNAME -p '$script.PASSWORD' -n $sast_project_name -d $sast_reports_output_dir
+                            """
+                        }
+                }
+            }
+        }
+
+        // Create tar.gz archive from reports dir
+        script.sh "tar -czf ./${reports_dir}.tar.gz ${reports_dir}"
+        // Add generated file as archive artifact to build
+        script.archiveArtifacts artifacts: "${reports_dir}.tar.gz"
+
+        // Send generated archive to users via email
+        if (email_list != "") {
+            script.emailext (
+                        subject: "Checkmarx SAST reports from Jenkins build (${script.env.BUILD_TAG})",
+                        body: """\
+                        Вы получили это сообщение потому что ваш email был указан в списке рассылки
+                        для получения отчета Checkmarx SAST в рамках запуска сборки Jenkins:
+                        ${script.env.RUN_DISPLAY_URL} 
+
+                        Отчет для данной сборки вы сможете найти во вложении к данному письму, а так же по ссылке:
+                        ${script.env.RUN_ARTIFACTS_DISPLAY_URL}
+                        """.stripIndent(),
+                        attachmentsPattern: "${reports_dir}.tar.gz",
+                        to: email_list
+                    )
         }
     }
 }
